63fb82d045fcda8946818765374fd19aa5e917b0
aluau070722129002 <148319758+aluau070722129002@users.noreply.github.com>
2023-10-18 14:43:09 +0530

    Add files via upload

--- unit 1/challenge1.2.py
+++ unit 1/challenge1.2.py
@@ -1,9 +1,21 @@
-m1=int(input("Enter the marks 1:"))
-m2=int(input("Enter the mark 2:"))
-m3=int(input("Enter the mark 3:"))
-m4=int(input("Enter the mark 4:"))
-m5=int(input("Enter the mark 5:"))
-total=m1+m2+m3+m4+m5
-per=total/5
-print("Total Marks:",total)
-print("Percentage Marks:",per)
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
